b8a0e970946f083983e2eeb0b66fe7b1a878ae97
Ryan Mui <reachryanmui@gmail.com>
2019-02-16 16:26:45 +0000

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,7 +1,7 @@
 require 'pry'
 # Make your shoe class here!
 class Shoe
-  attr_accessor :color, :size, :material, :condition#, :cobble
+  attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
   def initialize(brand)
